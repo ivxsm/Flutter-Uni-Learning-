f2e6f3957a88c4bc54a21ba28ae30cdd6e326d50
Khaled Mustafa Al-Ghilan <kghilan@sm.imamu.edu.sa>
2024-12-01 17:41:06 +0300

    inint day1

---
+++ Day 1/only_dart/bin/only_dart.dart
@@ -0,0 +1,5 @@
+import 'package:only_dart/only_dart.dart' as only_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${only_dart.calculate()}!');
+}
---
+++ Day 1/only_dart/test/only_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:only_dart/only_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
