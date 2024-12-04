b6ab58ad1414a2b810478f7c2f7708241a0005a5
Khaled Mustafa Al-Ghilan <kghilan@sm.imamu.edu.sa>
2024-12-04 14:52:39 +0300

    tesing

---
+++ Day 2/HomeWork2/test.dart
@@ -0,0 +1,11 @@
+// this will be a simple test for git and github.
+
+String welcome (String name) {
+  return "Thanks for using this file $name";
+}
+
+
+void main (){
+
+  print(welcome("Khalied Mustafa"));
+}
