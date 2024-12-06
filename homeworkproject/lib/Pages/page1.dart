1f8b5fa36eef312e4a1fb324fba13f0bfed41787
Khaled Mustafa Al-Ghilan <kghilan@sm.imamu.edu.sa>
2024-12-06 23:43:20 +0300

    page1 is done

---
+++ homeworkproject/lib/Pages/page1.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:homeworkproject/Pages/Page2.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NeverCome BAck Screen"),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Container(
+        child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             crossAxisAlignment: CrossAxisAlignment.stretch,
+             children:[
+            Icon(Icons.shopify_rounded , size: 100, color: Colors.orange[300], ),
+            const Text('Welcome To The Gift Shop', textAlign : TextAlign.center, style: TextStyle(height: 2 , fontSize: 20, decoration: TextDecoration.overline ,)),
+            Align( alignment: Alignment.center, child:  ElevatedButton(onPressed: () { Navigator.push(context, MaterialPageRoute(builder: (context)=> const Page2()));}, child: const Text("Send a Gift !" , style: TextStyle(fontSize: 17 , wordSpacing: 0.5 , color: Color.fromARGB(219, 16, 16, 16)),),),)
+          ],
+        ),
+        ),
+    );
+  }
+}
--- homeworkproject/lib/main.dart
+++ homeworkproject/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:homeworkproject/Pages/page1.dart';
 
 void main() {
   runApp(const MainApp());
@@ -6,9 +7,11 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: Page1(),
+    );
   }
 }
