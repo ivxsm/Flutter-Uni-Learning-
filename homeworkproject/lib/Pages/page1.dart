661851cf3563915f2f106a194d25751a1347a13e
Khaled Mustafa Al-Ghilan <kghilan@sm.imamu.edu.sa>
2024-12-07 21:57:47 +0300

    homework project is done , will add some screen shots

--- homeworkproject/lib/Pages/page1.dart
+++ homeworkproject/lib/Pages/page1.dart
@@ -8,20 +8,47 @@ class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("NeverCome BAck Screen"),
+        title: Text("Never Come BAck ScreEn"),
         backgroundColor: Colors.amberAccent,
+        automaticallyImplyLeading: false, // to hide the Back button
       ),
       body: Container(
         child: Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-             crossAxisAlignment: CrossAxisAlignment.stretch,
-             children:[
-            Icon(Icons.shopify_rounded , size: 100, color: Colors.orange[300], ),
-            const Text('Welcome To The Gift Shop', textAlign : TextAlign.center, style: TextStyle(height: 2 , fontSize: 20, decoration: TextDecoration.overline ,)),
-            Align( alignment: Alignment.center, child:  ElevatedButton(onPressed: () { Navigator.push(context, MaterialPageRoute(builder: (context)=> const Page2()));}, child: const Text("Send a Gift !" , style: TextStyle(fontSize: 17 , wordSpacing: 0.5 , color: Color.fromARGB(219, 16, 16, 16)),),),)
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(
+              Icons.shopify_rounded,
+              size: 100,
+              color: Colors.orange[300],
+            ),
+            const Text('Welcome To The Gift Shop',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  height: 2,
+                  fontSize: 20,
+                  decoration: TextDecoration.overline,
+                )),
+            Align(
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Page2()));
+                },
+                child: const Text(
+                  "Send a Gift !",
+                  style: TextStyle(
+                      fontSize: 17,
+                      wordSpacing: 0.5,
+                      color: Color.fromARGB(219, 16, 16, 16)),
+                ),
+              ),
+            )
+            // did all this because i could not make everything in the center , it worked>.>
           ],
         ),
-        ),
+      ),
     );
   }
 }
--- homeworkproject/lib/Pages/page2.dart
+++ homeworkproject/lib/Pages/page2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:homeworkproject/Pages/page1.dart';
 
 class Page2 extends StatelessWidget {
   const Page2({super.key});
@@ -7,8 +8,72 @@ class Page2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Page 2"),
-        automaticallyImplyLeading: false,
+        title: const Text("Gift Page"),
+        automaticallyImplyLeading: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const SizedBox(
+              // added sizedBox to make the header smaller
+              height: 80.0, // here is the fix size.
+              child: DrawerHeader(
+                margin: EdgeInsets.all(
+                    10.0), // added some margin and padding to move the items a bit to the center and it worked !
+                padding: EdgeInsets.all(10.0),
+                child: Text('Where to go ?', style: TextStyle(fontSize: 17)),
+              ),
+            ),
+            ListTile(
+              title: const Text('Send a gift'),
+              onTap: () {
+                Navigator.pop(context); // stay in the same page
+              },
+            ),
+            ListTile(
+              title: const Text('Soon'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Page1()));
+              }, // go to the main page
+            )
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+            height: 400,
+            width: 400,
+            color: Colors.black87,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text('Welcome',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.amberAccent,
+                        fontWeight: FontWeight.w700)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Icon(
+                      Icons.card_giftcard,
+                      color: Colors.white,
+                    ),
+                    const Text("This is a gift",
+                        style: TextStyle(
+                          color: Colors.white,
+                        )),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Send!"),
+                    )
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
