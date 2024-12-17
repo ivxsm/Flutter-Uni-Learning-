4bdbaf83df4276ee79afd318106d0f30237270b9
Khaled Mustafa Al-Ghilan <121107036+ivxsm@users.noreply.github.com>
2024-12-17 23:19:48 +0300

    added env and edited gitignore

--- day9/lib/main.dart
+++ day9/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:day9/pages/pageone.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main()async {
+
+   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
+  try {
+    await dotenv.load(fileName: ".env"); // Load environment variables
+  } catch (e) {
+    throw Exception('Error loading .env file: $e'); // Print error if any
+  }
   runApp(const MainApp());
 }
 
